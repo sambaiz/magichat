1c50362e7189332e7c8aa0684dcd87b1b6f7a177
sambaiz <godgourd@gmail.com>
2014-11-24 00:42:39 +0900

    world has the_end which is false by default

--- spec/models/world_spec.rb
+++ spec/models/world_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe World, :type => :model do
     world.valid?
     expect(world.errors[:code]).to include("can't be blank")
   end
+
+  it "has the_end which is false by default" do
+    world = build(:world, code: nil)
+    expect(world.the_end).to be false
+  end
 end
