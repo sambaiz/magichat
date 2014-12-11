cc51da979abf50ab63872cb34e91aaa06e7382eb
sambaiz <godgourd@gmail.com>
2014-12-12 02:39:17 +0900

    move

--- app/assets/javascripts/chat.js.coffee
+++ app/assets/javascripts/chat.js.coffee
@@ -18,14 +18,42 @@ class @ChatClass
   pressKey: (event) =>
     if event.which == 13
       @sendMessage(event)
-      false
+      return false
+    if @move_mode
+      console.log event.which
+      if event.which == 106 # down
+        console.log('down')
+        @send(':down')
+      else if event.which == 104 # left
+        console.log('left')
+        @send(':left')
+      else if event.which == 107 # up
+        console.log('up')
+        @send(':up')
+      else if event.which == 108 # right
+        console.log('right')
+        @send(':right')
+      else
+        @move_mode = false
+        $('#msgbody').attr('placeholder','')
+      return false
 
   sendMessage: (event) =>
-    token = @generateToken()
-    user_name = $('#username').text()
     msg_body = $('#msgbody').val()
-    @dispatcher.trigger 'new_message', { world_code: @world_code, new_token: token, body: msg_body }
+    if msg_body == ':move'
+      console.log 'move mode'
+      @move_mode = true
+      $('#msgbody').val('')
+      $('#msgbody').attr('placeholder', 'move mode (hjklで操作、それ以外のキーを押したら終了)')
+      return false
+    msg_body = $('#msgbody').val()
+    @send(msg_body)
     $('#msgbody').val('')
+
+  send: (msg_body) =>
+    token = @generateToken()
+    @dispatcher.trigger 'new_message', { world_code: @world_code, new_token: token, body: msg_body }
+
 
   receiveMessage: (message) =>
     console.log message
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,9 @@ class Post < ActiveRecord::Base
   validates :point_y, presence: true
   validates :point_z, presence: true
 
-  def messages
+  def messages(exe_magic = true) #ログを辿るときにはexe_magic = Falseで呼ぶ必要がある
     if self.text.start_with?(":")
-      Magic.new(self.text[1..-1], self.user, self.world).effects
+      Magic.new(self.text[1..-1], self.user, self.world, exe_magic).effects
     else
       [{:type => :plain, :message => self.text}]
     end
@@ -16,10 +16,13 @@ class Post < ActiveRecord::Base
 end
 
 class Magic
-  def initialize(spell, user, world)
+  def initialize(spell, user, world, exe_magic)
     @effects = []
+    @user = user
+    @world = world
+    @exe_magic = exe_magic
     begin
-      method(spell).call(user, world)
+      method(spell).call
     rescue
     end
   end
@@ -28,8 +31,50 @@ class Magic
     @effects
   end
 
-  def fire(user, world)
-    @effects << {:type => :effect, :message => "#{user.name}はファイアを唱えた。"}
-    @effects << {:type => :effect, :message => "しかし効果はなかった！"}
+  def fire
+    @effects << {:type => :effect, :message => "#{@user.name}はファイアを唱えた。"}
+    if @exe_magic
+      @effects << {:type => :effect, :message => "しかし効果はなかった！"}
+    end
+  end
+
+  def up
+    if @exe_magic
+      @user.point_y -= 1
+      @user.save
+      @effects << {:type => :plain, :message => "#{@user.name}は北に移動した。(#{@user.point_x}, #{@user.point_y}, #{@user.point_z})"}
+    else
+      @effects << {:type => :plain, :message => "#{@user.name}は北に移動した。"}
+    end
+  end
+
+  def down
+    if @exe_magic
+      @user.point_y += 1
+      @user.save
+      @effects << {:type => :plain, :message => "#{@user.name}は南に移動した。(#{@user.point_x}, #{@user.point_y}, #{@user.point_z})"}
+    else
+      @effects << {:type => :plain, :message => "#{@user.name}は南に移動した。"}
+    end
+  end
+
+  def left
+    if @exe_magic
+      @user.point_x -= 1
+      @user.save
+      @effects << {:type => :plain, :message => "#{@user.name}は西に移動した。(#{@user.point_x}, #{@user.point_y}, #{@user.point_z})"}
+    else
+      @effects << {:type => :plain, :message => "#{@user.name}は西に移動した。"}
+    end
+  end
+
+  def right
+    if @exe_magic
+      @user.point_x += 1
+      @user.save
+      @effects << {:type => :plain, :message => "#{@user.name}は東に移動した。(#{@user.point_x}, #{@user.point_y}, #{@user.point_z})"}
+    else
+      @effects << {:type => :plain, :message => "#{@user.name}は東に移動した。"}
+    end
   end
 end
